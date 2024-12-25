993bccf3757354b16a2c46aeac2b4d04aa937832
leoluizgo <bolsistarema3@gmail.com>
2024-12-25 20:10:02 -0300

    update: change Horarios and InfoWrapper css files to tailwind

--- matrufsc/src/components/horarios/Horarios.tsx
+++ matrufsc/src/components/horarios/Horarios.tsx
@@ -1,8 +1,7 @@
 import { Fragment, useState } from "react";
-import styles from "./Horarios.module.css";
 
 export default function Horarios() {
-    const [isMostrarSalas, setMostrarSalas] = useState(false)
+    const [isMostrarSalas, setMostrarSalas] = useState(false);
 
     const DIAS = ["Segunda", "Ter\u00e7a", "Quarta", "Quinta", "Sexta", "S\u00e1bado"];
     const HORAS = ["07:30", "08:20", "09:10", "10:10", "11:00",
@@ -13,58 +12,50 @@ export default function Horarios() {
                 "19:20", "20:10", "21:10", "22:00"];
 
     const handleMostrarSalas = () => {
-        setMostrarSalas(!isMostrarSalas)
-    }
+        setMostrarSalas(!isMostrarSalas);
+    };
 
     return (
-        <table className={styles.table}>
-            <thead className={styles.head}>
+        <table
+            className="border border-black table-fixed w-full max-w-screen-lg border-separate border-spacing-1"
+            style={{ tableLayout: "fixed" }}
+        >
+            <thead>
                 <tr>
-                    <th>
-                        <input title="Mostrar Salas" type="checkbox" onClick={handleMostrarSalas}/>
+                    <th className="table-cell text-center w-12">
+                        <input title="Mostrar Salas" type="checkbox" onClick={handleMostrarSalas} />
                     </th>
-                    {
-                        DIAS.map((dia, index) => {
-                            return (
-                                <th key={index}>
-                                    {dia}
-                                </th>
-                            )
-                        })
-                    }
+                    {DIAS.map((dia, index) => (
+                        <th key={index} className="table-cell text-center w-24">
+                            {dia}
+                        </th>
+                    ))}
                 </tr>
             </thead>
-            <tbody className={styles.body}>
-                {                        
-                    HORAS.map((hora, index) => {
-                        return (
-                            <Fragment key={index}>
-                                {
-                                    hora == "13:30" || hora == "18:30" ? <tr style={{height: "4px"}}></tr> : null
-                                }
-                                <tr>
-                                    <td>
-                                        <div>{hora}</div>
-                                        {
-                                            isMostrarSalas ?
-                                                <div style={{display: "block"}}>
-                                                    {HORAS_FIM[index]}
-                                                </div>
-                                            :  
-                                                null
-                                        }
-                                    </td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                </tr>
-                            </Fragment>
-                        )
-                    })
-                }
+            <tbody>
+                {HORAS.map((hora, index) => (
+                    <Fragment key={index}>
+                        {hora === "13:30" || hora === "18:30" ? (
+                            <tr style={{ height: "4px" }}></tr>
+                        ) : null}
+                        <tr>
+                            <td className="table-cell text-center text-sm p-1">
+                                <div>{hora}</div>
+                                {isMostrarSalas && (
+                                    <div className="text-xs text-gray-600">
+                                        {HORAS_FIM[index]}
+                                    </div>
+                                )}
+                            </td>
+                            {DIAS.map((_, dayIndex) => (
+                                <td
+                                    key={dayIndex}
+                                    className="bg-white border border-black text-black text-sm h-8 text-center"
+                                ></td>
+                            ))}
+                        </tr>
+                    </Fragment>
+                ))}
             </tbody>
         </table>
     );
