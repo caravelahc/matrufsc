63a42fd26537cf3e819d32274ebffb998ef53338
pablolteixeira <xpspablo@hotmail.com>
2024-12-24 23:51:42 -0300

    refactor: move ui_logger.js code to react

---
+++ matrufsc/src/components/logger/Logger.tsx
@@ -0,0 +1,10 @@
+function Logger() {
+
+    return (
+        <div className="ui_logger">
+            <p>&lt;&lt;&lt;&lt; procure as disciplinas por nome ou código</p>
+        </div>
+    )
+}
+
+export default Logger
