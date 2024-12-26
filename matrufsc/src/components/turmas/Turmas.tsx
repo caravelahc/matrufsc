cdc1f4bf82ced13f8582a9e00c77c2d6a4ae1948
pablolteixeira <xpspablo@hotmail.com>
2024-12-26 05:27:17 -0300

    refactor: create css module file and improve turmas ui

--- matrufsc/src/components/turmas/Turmas.tsx
+++ matrufsc/src/components/turmas/Turmas.tsx
@@ -1,9 +1,10 @@
+import styles from "./Turmas.module.css"
+
 function Turmas() {
 
     return (
-        <>
-
-        </>
+        <div className={styles.ui_turmas} style={{overflow: "auto", position: "relative", width: "438px", height: "374px", maxHeight: "374px"}}>
+        </div>
     )
 }
 
