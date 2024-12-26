25995d01ddbca90edd49224c21dd0ca305833bb3
pablolteixeira <xpspablo@hotmail.com>
2024-12-26 05:28:00 -0300

    refactor: create css module file and improve materias ui

--- matrufsc/src/components/materias/Materias.tsx
+++ matrufsc/src/components/materias/Materias.tsx
@@ -1,38 +1,42 @@
+import styles from "./Materias.module.css"
+
 function Materias() {
 
     return (
-        <table cellPadding={1} cellSpacing={1}>
-            <thead>
-                <tr style={{backgroundColor: "rgb(238, 238, 238)"}}>
-                    <th style={{width: "22px"}}>
-                    </th>
-                    <th style={{textAlign: "center", width: "60px"}}>
-                        Código
-                    </th>
-                    <th style={{textAlign: "center", width: "50px"}}>
-                        Turma
-                    </th>
-                    <th style={{textAlign: "center", width: "60px"}}>
-                        Período
-                    </th>
-                    <th id="combinacoes" className="ui_combinacoes">
-                        Combinações 
-                        <span title="combinação anterior">
-                            <strong>
-                                &nbsp;&lt;&nbsp;
-                            </strong>
-                        </span> 
-                        <input />/0 
-                        <span title="próxima combinação">
-                            <strong>
-                                &nbsp;&gt;&nbsp;
-                            </strong>
-                        </span> 
-                        Créditos por semana: 0
-                    </th>
-                </tr>
-            </thead>
-        </table>
+        <div className={styles.ui_materias}>
+            <table cellPadding={1} cellSpacing={1}>
+                <thead>
+                    <tr style={{backgroundColor: "rgb(238, 238, 238)"}}>
+                        <th style={{width: "22px"}}>
+                        </th>
+                        <th style={{textAlign: "center", width: "60px"}}>
+                            Código
+                        </th>
+                        <th style={{textAlign: "center", width: "50px"}}>
+                            Turma
+                        </th>
+                        <th style={{textAlign: "center", width: "60px"}}>
+                            Período
+                        </th>
+                        <th id="combinacoes" className={styles.ui_combinacoes}>
+                            Combinações 
+                            <span title="Combinação Anterior">
+                                <strong>
+                                    &nbsp;&lt;&nbsp;
+                                </strong>
+                            </span> 
+                            <input type="text" value={0}/>/0 
+                            <span title="Próxima Combinação">
+                                <strong>
+                                    &nbsp;&gt;&nbsp;
+                                </strong>
+                            </span> 
+                            Créditos por semana: 0
+                        </th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
     )
 }
 
