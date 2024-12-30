760d8c2998c0c65f0d6022fdc6ca13ca09839ee0
pablolteixeira <xpspablo@hotmail.com>
2024-12-30 14:07:25 -0300

    feat: implement horarios page

--- matrufsc/src/components/horarios/Horarios.tsx
+++ matrufsc/src/components/horarios/Horarios.tsx
@@ -1,16 +1,53 @@
 import { Fragment, useState } from "react";
 import styles from "./Horarios.module.css";
+import { IMateria } from "../../utils/materiasOperations";
 
-export default function Horarios() {
+type HorarioData = {
+    codigo: string,
+    cor: string,
+    centro: string,
+    sala: string
+}
+
+export default function Horarios(
+    {
+        selectedMaterias
+    } : {
+        selectedMaterias : Array<IMateria>
+    }) {
     const [isMostrarSalas, setMostrarSalas] = useState(false)
 
     const DIAS = ["Segunda", "Ter\u00e7a", "Quarta", "Quinta", "Sexta", "S\u00e1bado"];
     const HORAS = ["07:30", "08:20", "09:10", "10:10", "11:00",
         "13:30", "14:20", "15:10", "16:20", "17:10",
-        "18:30", "19:20", "20:20", "21:10"];
+        "18:30", "19:20", "20:20", "21:10"]
+
     const HORAS_FIM = ["08:20", "09:10", "10:00", "11:00", "11:50",
                 "14:20", "15:10", "16:00", "17:10", "18:00",
-                "19:20", "20:10", "21:10", "22:00"];
+                "19:20", "20:10", "21:10", "22:00"]
+    
+    const arrayHorarios = Array.from({ length: HORAS.length }, () => new Array<HorarioData | null>(7).fill(null))
+
+    for (const selectedMateria of selectedMaterias) {
+        if (selectedMateria.isChecked) {
+            const turma = selectedMateria.turmas.find(turma => turma.codigo == selectedMateria.turmaEscolhida)
+    
+            if (turma) {
+                for (const horario of turma?.horarios) {
+                    const index = HORAS.findIndex(hora => hora == horario.inicio)
+    
+                    for (let i = 0; i < horario.aulas; i++) {
+                        arrayHorarios[index + i][horario.dia - 2] = {
+                            codigo: selectedMateria.codigo,
+                            cor: selectedMateria.cor || "white",
+                            centro: horario.centro,
+                            sala: horario.sala
+                        }
+                    }
+                }
+            }    
+        }
+    }
 
     const handleMostrarSalas = () => {
         setMostrarSalas(!isMostrarSalas)
@@ -55,12 +92,30 @@ export default function Horarios() {
                                                     null
                                             }
                                         </td>
-                                        <td className={styles.ui_horario_celula} style={{backgroundColor: "white", border: "1px solid black", color: "black"}}></td>
-                                        <td className={styles.ui_horario_celula} style={{backgroundColor: "white", border: "1px solid black", color: "black"}}></td>
-                                        <td className={styles.ui_horario_celula} style={{backgroundColor: "white", border: "1px solid black", color: "black"}}></td>
-                                        <td className={styles.ui_horario_celula} style={{backgroundColor: "white", border: "1px solid black", color: "black"}}></td>
-                                        <td className={styles.ui_horario_celula} style={{backgroundColor: "white", border: "1px solid black", color: "black"}}></td>
-                                        <td className={styles.ui_horario_celula} style={{backgroundColor: "white", border: "1px solid black", color: "black"}}></td>
+                                        {
+                                            arrayHorarios[index].map(horario => {
+                                                return (
+                                                    horario === null ?
+                                                        (
+                                                            <td className={styles.ui_horario_celula} style={{backgroundColor: "white", border: "1px solid black", color: "black"}}></td>
+                                                        )
+                                                    :
+                                                        (
+                                                            <td className={styles.ui_horario_celula} style={{backgroundColor: horario.cor, border: "1px solid black", color: "black"}}>
+                                                                {horario.codigo}
+                                                                {
+                                                                    isMostrarSalas ?
+                                                                        <div style={{fontSize: "10px"}}>
+                                                                            {horario.centro}-{horario.sala}
+                                                                        </div>
+                                                                    :
+                                                                        null
+                                                                }
+                                                            </td>
+                                                        )
+                                                )
+                                            })
+                                        }
                                     </tr>
                                 </Fragment>
                             )
